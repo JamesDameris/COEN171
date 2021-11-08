6595f387ddab97fff84a7381d890c8d9feb875de
james t dameris <jdameris@linux10610.dc.engr.scu.edu>
2021-11-08 15:55:00 -0800

    part 1 done

--- hw3/sort.scm
+++ hw3/sort.scm
@@ -1,19 +1,18 @@
 
-(define(filter f lst)
+(define (filter f lst)
     (cond
-        ((null? lst) lst
+        ((null? lst) lst)
         ((f(car lst))
-         (cons(car lst)(filter f (cdr lst)))))
+         (cons (car lst) (filter f (cdr lst))))
         (else (filter f (cdr lst)))))
-
 
 (define(quicksort lst)
     (if (null? lst) '()
         (let ((x (car lst))
               (xs (cdr lst)) )
               (append (quicksort 
-                            (filter (f(x')(< x' x)) xs))
+                            (filter (lambda(y)(< y x)) xs))
               (list x) 
               (quicksort
-                    (filter (f(x')(>= x' x)) xs))))))
+                    (filter (lambda(y)(>= y x)) xs))))))
                         
